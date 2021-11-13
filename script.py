fbe0c3a30dc28c53f495fa29f6a9ca7fc6b3f33f
leovct <l009.vincent@gmail.com>
2021-11-13 22:23:34 +0100

    update script

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-# Generates random rose mathematical patterns using colors from palettes.
+# Generates random rose mathematical patterns using colours from palettes.
 # The palettes are deployed on the Ethereum blockchain (see palettes.io).
 # The images are generated as svg files and can be stored on-chain.
 # More information can be found on the pattern: https://en.wikipedia.org/wiki/Rose_%28mathematics%29
@@ -8,19 +8,19 @@ import random
 
 
 class RosePattern:
-    def __init__(self, _name, _img_length, _background_color, _shape_colors, _point_radius,
-                 _color_line_length, _step):
+    def __init__(self, _name, _img_length, _background_colour, _shape_colours, _point_radius,
+                 _colour_line_length, _step):
         self.name = _name
         self.img_length = _img_length
-        self.background_color = _background_color
-        self.shape_colors = _shape_colors
+        self.background_colour = _background_colour
+        self.shape_colours = _shape_colours
         self.point_radius = _point_radius
-        self.color_line_length = _color_line_length
+        self.colour_line_length = _colour_line_length
         self.step = _step
 
     def _generate_background(self):
         return '<rect x="{0}" y="{0}" width="{1}" height="{1}" fill="{2}"></rect>'.format(
-            -self.img_length, 2*self.img_length, self.background_color)
+            -self.img_length, 2*self.img_length, self.background_colour)
 
     def _generate_pattern(self, _n, _d):
         # A rose is the set of points in polar coordinates specified by the polar equation:
@@ -28,12 +28,12 @@ class RosePattern:
         a = self.img_length/2-20
         k = _n/_d
 
-        # The color of the points is randomly choosen and changes every x generated points (see the
-        # _color_line_length parameter)
-        last_palette_color_index = len(self.shape_colors)-1
-        number_points_with_same_color = 0
-        point_color = self.shape_colors[random.randint(
-            0, last_palette_color_index)]
+        # The colour of the points is randomly choosen and changes every x generated points (see the
+        # _colour_line_length parameter)
+        last_palette_colour_index = len(self.shape_colours)-1
+        number_points_with_same_colour = 0
+        point_colour = self.shape_colours[random.randint(
+            0, last_palette_colour_index)]
 
         points = []
         theta = 0.
@@ -47,18 +47,18 @@ class RosePattern:
 
             # Create the point using the svg format
             point = '<circle cx="{}" cy="{}" r="{}" fill="{}"></circle>'.format(
-                x, y, self.point_radius, point_color)
+                x, y, self.point_radius, point_colour)
             points.append(point)
 
             # Increment theta
             theta += self.step
 
-            # Check if the color of the point needs to be changed
-            number_points_with_same_color += 1
-            if number_points_with_same_color >= self.color_line_length:
-                number_points_with_same_color = 0
-                point_color = self.shape_colors[random.randint(
-                    0, last_palette_color_index)]
+            # Check if the colour of the point needs to be changed
+            number_points_with_same_colour += 1
+            if number_points_with_same_colour >= self.colour_line_length:
+                number_points_with_same_colour = 0
+                point_colour = self.shape_colours[random.randint(
+                    0, last_palette_colour_index)]
         # return all the points forming the rose pattern shape
         return points
 
@@ -82,30 +82,30 @@ class RosePattern:
 
 
 def main():
-    palette_colors = ["#44ffcc", "#bb7722", "#77bbee", "#9988cc", "#ff5566"]
+    palette_colours = ["#44ffcc", "#bb7722", "#77bbee", "#9988cc", "#ff5566"]
 
-    rose_black_bg = RosePattern("rose_black_bg", 1000, "#000000", palette_colors, 2, 20, 0.02)
+    rose_black_bg = RosePattern("rose_black_bg", 1000, "#000000", palette_colours, 2, 20, 0.02)
     rose_black_bg.generate()
 
-    rose_white_bg = RosePattern("rose_white_bg", 1000, "#ffffff", palette_colors, 2, 20, 0.02)
+    rose_white_bg = RosePattern("rose_white_bg", 1000, "#ffffff", palette_colours, 2, 20, 0.02)
     rose_white_bg.generate()
 
-    rose_black_bg_thin = RosePattern("rose_black_bg_thin", 1000, "#000000", palette_colors, 1, 20, 0.02)
+    rose_black_bg_thin = RosePattern("rose_black_bg_thin", 1000, "#000000", palette_colours, 1, 20, 0.02)
     rose_black_bg_thin.generate()
 
-    rose_white_bg_thin = RosePattern("rose_white_bg_thin", 1000, "#ffffff", palette_colors, 1, 20, 0.02)
+    rose_white_bg_thin = RosePattern("rose_white_bg_thin", 1000, "#ffffff", palette_colours, 1, 20, 0.02)
     rose_white_bg_thin.generate()
 
-    rose_black_bg_more_steps = RosePattern("rose_black_bg_more_steps", 1000, "#000000", palette_colors, 2, 20, 0.01)
+    rose_black_bg_more_steps = RosePattern("rose_black_bg_more_steps", 1000, "#000000", palette_colours, 2, 20, 0.01)
     rose_black_bg_more_steps.generate()
 
-    rose_white_bg_more_steps = RosePattern("rose_white_bg_more_steps", 1000, "#ffffff", palette_colors, 2, 20, 0.01)
+    rose_white_bg_more_steps = RosePattern("rose_white_bg_more_steps", 1000, "#ffffff", palette_colours, 2, 20, 0.01)
     rose_white_bg_more_steps.generate()
 
-    rose_black_bg_way_more_steps = RosePattern("rose_black_bg_way_more_steps", 1000, "#000000", palette_colors, 2, 20, 0.001)
+    rose_black_bg_way_more_steps = RosePattern("rose_black_bg_way_more_steps", 1000, "#000000", palette_colours, 2, 20, 0.001)
     rose_black_bg_way_more_steps.generate()
 
-    rose_white_bg_way_more_steps = RosePattern("rose_white_bg_way_more_steps", 1000, "#ffffff", palette_colors, 2, 20, 0.001)
+    rose_white_bg_way_more_steps = RosePattern("rose_white_bg_way_more_steps", 1000, "#ffffff", palette_colours, 2, 20, 0.001)
     rose_white_bg_way_more_steps.generate()
 
 
