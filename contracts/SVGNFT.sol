6fd0c6ffc01435d2b1add8c5e91b6fddc00a4c23
leovct <l009.vincent@gmail.com>
2021-11-14 01:53:55 +0100

    update natspec

--- contracts/SVGNFT.sol
+++ contracts/SVGNFT.sol
@@ -1,13 +1,15 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.10;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "./URI.sol";
 
-//// @title A contract to create a collection of NFTs that output SVG code in their URI
-//// @author leovct
-//// @dev Compliant with OpenZeppelin's implementation of the ERC721 spec draft
+/**
+ * @title A contract to create a collection of NFTs that output SVG code in their URI
+ * @author leovct
+ * @dev Compliant with OpenZeppelin's implementation of the ERC721 spec draft
+ */
 contract SVGNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
@@ -16,8 +18,10 @@ contract SVGNFT is ERC721URIStorage {
 
     constructor() ERC721("Scalable Vector Graphics NFT", "SVGNFT") {}
 
-    //// @notice Mint a SVG NFT
-    //// @param _svg the svg code of the NFT
+    /**
+     * @notice Mint a SVG NFT
+     * @param _svg the svg code of the NFT
+     */
     function mint(string memory _svg) public {
         uint256 id = _tokenIdCounter.current();
         _safeMint(msg.sender, id);
--- contracts/URI.sol
+++ contracts/URI.sol
@@ -1,22 +1,28 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.10;
 
 import "base64-sol/base64.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
-//// @title Provide useful functions to manipulate URIs to create NFTs
-//// @author leovct
+/**
+ * @title Provide useful functions to manipulate URIs to create NFTs
+ * @author leovct
+ */
 library URI {
-    //// @notice Create an image URI with SVG code embedded
-    //// @param _svg the svg code to embed in the URI
+    /**
+     * @notice Create an image URI with SVG code embedded
+     * @param _svg the svg code to embed in the URI
+     */
     function svgToImageURI(string memory _svg) internal pure returns (string memory) {
         string memory baseURL = "data:image/svg+xml;base64,";
         string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(_svg))));
         return string(abi.encodePacked(baseURL, svgBase64Encoded));
     }
 
-    //// @notice Format the token URI of the NFT
-    //// @param _imageURI the URI of the NFT
+    /**
+     * @notice Format the token URI of the NFT
+     * @param _imageURI the URI of the NFT
+     */
     function formatTokenURI(uint256 _tokenId, string memory _imageURI) internal pure returns (string memory) {
         string memory baseURL = "data:application/json;base64,";
         string memory tokenJsonBase64Encoded = Base64.encode(bytes(abi.encodePacked(    
