bbfd19303b7777a1eb8cccf02ba9e10786ce93f2
leovct <l009.vincent@gmail.com>
2021-11-14 14:50:38 +0100

    add rotate animation to rose svgs

--- rose-svg-generator.py
+++ rose-svg-generator.py
@@ -8,15 +8,16 @@ import random
 
 
 class RosePattern:
-    def __init__(self, _name, _img_length, _background_colour, _shape_colours, _point_radius,
-                 _colour_line_length, _step):
-        self.name = _name
-        self.img_length = _img_length
-        self.background_colour = _background_colour
-        self.shape_colours = _shape_colours
-        self.point_radius = _point_radius
-        self.colour_line_length = _colour_line_length
-        self.step = _step
+    def __init__(self, name, shape_colours, background_colour="#000000", img_length=1000,
+                 point_radius=2, colour_line_length=20, step=0.02, rotation_period=60):
+        self.name = name
+        self.shape_colours = shape_colours
+        self.background_colour = background_colour
+        self.img_length = img_length
+        self.point_radius = point_radius
+        self.colour_line_length = colour_line_length
+        self.step = step
+        self.rotation_period = rotation_period
 
     def _generate_background(self):
         return '<rect x="{0}" y="{0}" width="{1}" height="{1}" fill="{2}"></rect>'.format(
@@ -62,21 +63,37 @@ class RosePattern:
         # return all the points forming the rose pattern shape
         return points
 
+    def _generate_rotate_animation(self, _clockwise_direction=True):
+        rotateAnimation = '<animateTransform attributeType="xml" attributeName="transform" type="rotate" ' 
+        if _clockwise_direction:
+            rotateAnimation += 'from="0" to="360"'
+        else:
+            rotateAnimation += 'from="360" to="0"'
+        rotateAnimation += ' dur="{}s" additive="sum" repeatCount="indefinite" />"'.format(
+            self.rotation_period)
+        return rotateAnimation
+
     def generate(self):
         with open("svg/generated/{}.svg".format(self.name), 'w') as f:
-            f.write('<svg xmlns="http://www.w3.org/2000/svg" width="{0}" height="{0}" viewBox="{1} {1} {0} {0}">'.format(
-                self.img_length, -self.img_length/2))
+            f.write('<svg xmlns="http://www.w3.org/2000/svg" width="{0}" height="{0}" viewBox="' +
+                    '{1} {1} {0} {0}">'.format(self.img_length, -self.img_length/2))
 
             background = self._generate_background()
             f.write("\n\t"+background)
 
-            first_pattern = self._generate_pattern(7, 2)
+            f.write("\n\t<g>")
+            first_pattern = self._generate_pattern(7, 8)
             for shape in first_pattern:
-                f.write("\n\t"+shape)
+                f.write("\n\t\t"+shape)
+            f.write('\n\t\t'+self._generate_rotate_animation())
+            f.write('\n\t</g>')
 
-            second_pattern = self._generate_pattern(7, 8)
+            f.write("\n\t<g>")
+            second_pattern = self._generate_pattern(4, 5)
             for shape in second_pattern:
-                f.write("\n\t"+shape)
+                f.write("\n\t\t"+shape)
+            f.write('\n\t\t'+self._generate_rotate_animation(False))
+            f.write('\n\t</g>')
 
             f.write('\n\tSorry, your browser does not support inline SVG.')
             f.write('\n</svg>')
@@ -85,31 +102,44 @@ class RosePattern:
 def main():
     palette_colours = ["#44ffcc", "#bb7722", "#77bbee", "#9988cc", "#ff5566"]
 
-    rose_black_bg = RosePattern("rose_black_bg", 1000, "#000000", palette_colours, 2, 20, 0.02)
+    rose_black_bg = RosePattern(
+        name="rose_black_bg", shape_colours=palette_colours)
     rose_black_bg.generate()
 
-    rose_white_bg = RosePattern("rose_white_bg", 1000, "#ffffff", palette_colours, 2, 20, 0.02)
+    rose_white_bg = RosePattern(
+        name="rose_white_bg", shape_colours=palette_colours, background_colour="#ffffff")
     rose_white_bg.generate()
 
-    rose_black_bg_thin = RosePattern("rose_black_bg_thin", 1000, "#000000", palette_colours, 1, 20, 0.02)
+    rose_black_bg_thin = RosePattern(
+        name="rose_black_bg_thin", shape_colours=palette_colours, point_radius=1)
     rose_black_bg_thin.generate()
 
-    rose_white_bg_thin = RosePattern("rose_white_bg_thin", 1000, "#ffffff", palette_colours, 1, 20, 0.02)
+    rose_white_bg_thin = RosePattern(
+        name="rose_white_bg_thin", shape_colours=palette_colours, background_colour="#ffffff",
+        point_radius=1)
     rose_white_bg_thin.generate()
 
-    rose_black_bg_more_steps = RosePattern("rose_black_bg_more_steps", 1000, "#000000", palette_colours, 2, 20, 0.01)
+    rose_black_bg_more_steps = RosePattern(
+        name="rose_black_bg_more_steps", shape_colours=palette_colours, step=0.01)
     rose_black_bg_more_steps.generate()
 
-    rose_white_bg_more_steps = RosePattern("rose_white_bg_more_steps", 1000, "#ffffff", palette_colours, 2, 20, 0.01)
+    rose_white_bg_more_steps = RosePattern(
+        name="rose_white_bg_more_steps", shape_colours=palette_colours,
+        background_colour="#ffffff", step=0.01)
     rose_white_bg_more_steps.generate()
 
-    rose_black_bg_way_more_steps = RosePattern("rose_black_bg_way_more_steps", 1000, "#000000", palette_colours, 2, 20, 0.001)
+    rose_black_bg_way_more_steps = RosePattern(
+        name="rose_black_bg_way_more_steps", shape_colours=palette_colours, step=0.001)
     rose_black_bg_way_more_steps.generate()
 
-    rose_white_bg_way_more_steps = RosePattern("rose_white_bg_way_more_steps", 1000, "#ffffff", palette_colours, 2, 20, 0.001)
+    rose_white_bg_way_more_steps = RosePattern(
+        name="rose_white_bg_way_more_steps", shape_colours=palette_colours,
+        background_colour="#ffffff", step=0.001)
     rose_white_bg_way_more_steps.generate()
 
-    rose_black_bg_optimised = RosePattern("rose_black_bg_optimised", 500, "#000000", palette_colours, 1, 20, 0.01)
+    rose_black_bg_optimised = RosePattern(
+        name="rose_black_bg_optimised", shape_colours=palette_colours, img_length=500,
+        point_radius=1)
     rose_black_bg_optimised.generate()
 
 
